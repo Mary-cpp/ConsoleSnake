a96f452f1fee84eba6e525c7e443dfa3390934b3
Maria_Soproniuk <f13x@rasengan.ru>
2022-05-13 23:13:48 +0300

    Class Point created

--- ConsoleSnake/Program.cs
+++ ConsoleSnake/Program.cs
@@ -4,9 +4,17 @@ namespace ConsoleSnake
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point x1 = new Point();
+            x1.x = 1;
+            x1.y = 3;
+            x1.sym = '*';
+
+            x1.Draw();
+
+            Console.ReadLine();
         }
     }
 }
